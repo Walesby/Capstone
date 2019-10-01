3f072539c54e938245ed1a8490282cbfb9ff6c24
David Walesby <david.j.walesby@gmail.com>
2019-10-01 16:12:12 -0400

    Added webpages and updated nav bar to proper routes

---
+++ Controllers/MangaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone.Controllers
+{
+    public class MangaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Search()
+        {
+            return View();
+        }
+        public IActionResult Reviews()
+        {
+            return View();
+        }
+        public IActionResult Top()
+        {
+            return View();
+        }
+    }
+}
